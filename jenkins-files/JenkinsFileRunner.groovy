9b8ee7fa9ffb2d6c0d5608af6e48e6a7c4a2e1de
Andrian98 <andrian.moisei@endava.com>
2024-08-05 10:39:27 +0300

    added the instalation of the maven

--- jenkins-files/JenkinsFileRunner.groovy
+++ jenkins-files/JenkinsFileRunner.groovy
@@ -17,44 +17,63 @@ pipeline {
     }
 
     stages {
-        stage('Checkout') {
+        stage('Setup Maven') {
             steps {
-                git branch: 'performance_atf', url: 'https://github.com/Andrian98/master_atf.git'
+                script {
+                    // Check if Maven is installed
+                    def mvnVersion = sh(script: 'mvn -version', returnStatus: true)
+                    if (mvnVersion != 0) {
+                        // Install Maven if not found
+                        sh '''
+                            echo "Maven not found. Installing Maven..."
+                            wget https://downloads.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.zip
+                            unzip apache-maven-3.8.6-bin.zip -d /opt
+                            ln -s /opt/apache-maven-3.8.6/bin/mvn /usr/bin/mvn
+                        '''
+                    } else {
+                        echo "Maven is already installed."
+                    }
+                }
+            }
+
+            stage('Checkout') {
+                steps {
+                    git branch: 'performance_atf', url: 'https://github.com/Andrian98/master_atf.git'
+                }
+            }
+
+            stage('Build') {
+                steps {
+                    sh 'mvn clean install'
+                }
+            }
+
+            stage('Test') {
+                steps {
+                    def runner = params.RUNNER
+                    sh "mvn test -Dtest=${runner}"
+                }
+            }
+
+            stage('Generate Reports') {
+                steps {
+                    cucumber buildStatus: 'UNSTABLE', fileIncludePattern: 'target/cucumber-reports/*.json'
+                }
             }
         }
 
-        stage('Build') {
-            steps {
-                sh 'mvn clean install'
-            }
-        }
-
-        stage('Test') {
-            steps {
-                def runner = params.RUNNER
-                sh "mvn test -Dtest=${runner}"
-            }
-        }
-
-        stage('Generate Reports') {
-            steps {
+        post {
+            always {
+                junit 'target/surefire-reports/*.xml'
                 cucumber buildStatus: 'UNSTABLE', fileIncludePattern: 'target/cucumber-reports/*.json'
+                archiveArtifacts artifacts: 'target/*.jar', allowEmptyArchive: true
+
+                emailext(
+                        subject: "Build ${currentBuild.fullDisplayName} - ${currentBuild.result}",
+                        body: "Check Jenkins for the detailed results of the build.",
+                        to: 'andrian.moisei@endava.com', // recipient email address
+                        recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']]
+                )
             }
         }
     }
-
-    post {
-        always {
-            junit 'target/surefire-reports/*.xml'
-            cucumber buildStatus: 'UNSTABLE', fileIncludePattern: 'target/cucumber-reports/*.json'
-            archiveArtifacts artifacts: 'target/*.jar', allowEmptyArchive: true
-
-            emailext(
-                    subject: "Build ${currentBuild.fullDisplayName} - ${currentBuild.result}",
-                    body: "Check Jenkins for the detailed results of the build.",
-                    to: 'andrian.moisei@endava.com', // recipient email address
-                    recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']]
-            )
-        }
-    }
-}
