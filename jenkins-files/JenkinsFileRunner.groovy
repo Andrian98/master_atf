d1009126b905d9224685541a7105e175e4061d82
Andrian98 <andrian.moisei@endava.com>
2024-08-05 11:06:12 +0300

    changed the sh to bat to be executable on windows

--- jenkins-files/JenkinsFileRunner.groovy
+++ jenkins-files/JenkinsFileRunner.groovy
@@ -41,7 +41,7 @@ pipeline {
 
             stage('Build') {
                 steps {
-                    sh 'mvn clean install'
+                    bat 'mvn clean install'
                 }
             }
 
