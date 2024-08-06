eb94c40417fa26ee0ab35f62e2da0ff5115e7ad9
Andrian98 <andrian.moisei@endava.com>
2024-08-06 16:06:19 +0300

    Deleted all the implimentation class for the UI

--- src/test/java/performance/upskilling/atf/ui/pageobjects/TransferPageElements.java
+++ src/test/java/performance/upskilling/atf/ui/pageobjects/TransferPageElements.java
@@ -1,7 +1,5 @@
 package performance.upskilling.atf.ui.pageobjects;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -14,7 +12,6 @@ import performance.upskilling.atf.configuration.driverfactory.WebDriverWaiter;
 public class TransferPageElements {
     private WebDriver driver;
     private WebDriverWait wait;
-    public static final Logger logger = LogManager.getLogger();
 
     @FindBy(id = "amount")
     private WebElement amount;
