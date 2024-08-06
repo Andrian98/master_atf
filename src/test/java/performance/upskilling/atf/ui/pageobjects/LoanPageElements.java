c70e294b556aac5a5c3ef2c01016d58b2a7acf67
Andrian98 <andrian.moisei@endava.com>
2024-08-06 16:04:20 +0300

    Improvements related to the impl. class

--- src/test/java/performance/upskilling/atf/ui/pageobjects/LoanPageElements.java
+++ src/test/java/performance/upskilling/atf/ui/pageobjects/LoanPageElements.java
@@ -1,21 +1,26 @@
 package performance.upskilling.atf.ui.pageobjects;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import performance.upskilling.atf.configuration.driverfactory.WebDriverManager;
 import performance.upskilling.atf.configuration.driverfactory.WebDriverWaiter;
+import performance.upskilling.atf.util.TestCustomActions;
+
+import java.util.Map;
 
 public class LoanPageElements {
     private WebDriver driver;
     private WebDriverWait wait;
+    public static final Logger logger = LogManager.getLogger();
+    private static final TestCustomActions testCustomActions = new TestCustomActions();
 
     @FindBy(id = "amount")
     private WebElement loanAmountField;
-
 
     @FindBy(id = "downPayment")
     private WebElement downPaymentField;
@@ -23,7 +28,6 @@ public class LoanPageElements {
     //Locate the "From Account" dropdown
     @FindBy(id = "fromAccountId")
     private WebElement fromAccountDropdown;
-
 
     @FindBy(xpath = "//input[@value='Apply Now']")
     private WebElement applyLoanButton;
@@ -62,5 +66,27 @@ public class LoanPageElements {
 
     public WebElement getNewAccountId() {
         return newAccountId;
+    }
+
+    public void populateLoanFields(Map<String, String> loanData) {
+        testCustomActions.sendKeysToWebElement(getLoanAmountField(), loanData.get("Loan Amount"));
+        testCustomActions.sendKeysToWebElement(getDownPaymentField(), loanData.get("Down Payment"));
+        logger.info("User entered loan details");
+    }
+
+    public void clickApplyNowButton() {
+        testCustomActions.clickButton(getApplyLoanButton());
+        logger.info("Clicks apply now button");
+    }
+
+    public void validateLoanRequest() {
+        String actualText = testCustomActions.getTextFromPage(getLoanText());
+        testCustomActions.assertPageText("Congratulations, your loan has been approved.", actualText);
+        logger.info("Validated loan request");
+    }
+
+    public void printNewAccountId() {
+        String newAccountId = getNewAccountId().getText();
+        logger.debug("New account number is {}", newAccountId);
     }
 }
--- src/test/java/performance/upskilling/atf/ui/steps/RequestLoanSteps.java
+++ src/test/java/performance/upskilling/atf/ui/steps/RequestLoanSteps.java
@@ -5,7 +5,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import performance.upskilling.atf.configuration.properties.PropertiesManager;
-import performance.upskilling.atf.ui.pagesimpl.RequestLoanImpl;
+import performance.upskilling.atf.ui.pageobjects.LoanPageElements;
 import performance.upskilling.atf.util.TestCustomActions;
 
 import java.util.Map;
@@ -13,7 +13,7 @@ import java.util.Map;
 public class RequestLoanSteps {
     public static TestCustomActions testCustomActions = new TestCustomActions();
     public static String requestLoanURL = PropertiesManager.getRequestLoanURL();
-    public static RequestLoanImpl requestLoanImpl = new RequestLoanImpl();
+    public static LoanPageElements loanPageElements = new LoanPageElements();
 
 
     @Given("user is on request loan page")
@@ -21,18 +21,17 @@ public class RequestLoanSteps {
         testCustomActions.navigateTo(requestLoanURL);
     }
 
-
     @When("user applies for a loan with the following details")
     public void userAppliesForALoanWithTheFollowingDetails(DataTable dataTable) {
         Map<String, String> data = dataTable.asMap();
-        requestLoanImpl.populateLoanFields(data);
-        requestLoanImpl.clickApplyNowButton();
+        loanPageElements.populateLoanFields(data);
+        loanPageElements.clickApplyNowButton();
     }
 
     @Then("new account number is provided")
     public void newAccountNumberIsProvided() {
-        requestLoanImpl.validateLoanRequest();
-        requestLoanImpl.printNewAccountId();
+        loanPageElements.validateLoanRequest();
+        loanPageElements.printNewAccountId();
     }
 
 }
